06d76f2a719233356d27b380b5e935d289d69f1f
Julian Valencia <valenciajuliann@hotmail.com>
2025-09-13 15:45:26 -0500

    Revert "malas practicas pa probar la proteccion de development"
    This reverts commit 3b63f4855b1de4a7974d4cd1aa38a6f157b6db52.

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,3 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index')
 
 // Language Change Route
 Route::post('/language/change', 'App\Http\Controllers\LanguageController@change')->name('language.change');
-
-
-fasdfasdfasdfasdfasdfasdfasdfads
