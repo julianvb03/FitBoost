09942fe4ac015a716e61eebece9e65b7eccba486
Julian Valencia <valenciajuliann@hotmail.com>
2025-09-13 16:14:40 -0500

    style(model) Run Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Collection;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -25,13 +26,12 @@ class User extends Authenticatable
      * $this->reviews - Review[] - contains the user's reviews
      * $this->tests - Test[] - contains the user's tests
      */
-
     protected $fillable = [
         'name',
         'email',
         'password',
         'address',
-        #'cardData',
+        // 'cardData',
     ];
 
     protected $hidden = [
@@ -49,23 +49,96 @@ class User extends Authenticatable
         ];
     }
 
-    getId(): int
+    public function getId(): int
     {
         return $this->attributes['id'];
     }
 
-    // public function orders(): HasMany
-    // {
-    //     return $this->hasMany(Order::class);
-    // }
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
 
-    // public function reviews(): HasMany
-    // {
-    //     return $this->hasMany(Review::class);
-    // }
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
 
-    // public function tests(): HasMany
-    // {
-    //     return $this->hasMany(Test::class);
-    // }
+    public function getAddress(): ?string
+    {
+        return $this->attributes['address'] ?? null;
+    }
+
+    public function getCardData(): ?string
+    {
+        return $this->attributes['cardData'] ?? null;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->attributes['address'] = $address;
+    }
+
+    public function setCardData(string $cardData): void
+    {
+        $this->attributes['cardData'] = $cardData;
+    }
+
+    // Eloquent Relationships
+    /*
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function setOrders(Collection $orders): void
+    {
+        $this->orders = $orders;
+    }
+
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function setReviews(Collection $reviews): void
+    {
+        $this->reviews = $reviews;
+    }
+
+    public function getReviews(): Collection
+    {
+        return $this->reviews;
+    }
+
+    public function tests(): HasMany
+    {
+        return $this->hasMany(Test::class);
+    }
+
+    public function setTests(Collection $tests): void
+    {
+        $this->tests = $tests;
+    }
+
+    public function getTests(): Collection
+    {
+        return $this->tests;
+    }
+    */
 }
