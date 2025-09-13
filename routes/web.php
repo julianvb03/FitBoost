3b63f4855b1de4a7974d4cd1aa38a6f157b6db52
Julian Valencia <valenciajuliann@hotmail.com>
2025-09-13 15:44:00 -0500

    malas practicas pa probar la proteccion de development

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index')
 
 // Language Change Route
 Route::post('/language/change', 'App\Http\Controllers\LanguageController@change')->name('language.change');
+
+
+fasdfasdfasdfasdfasdfasdfasdfads
