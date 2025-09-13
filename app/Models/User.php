0cb5db52e5423bc19b3d1333bb859b80d99e1b8e
Julian Valencia <valenciajuliann@hotmail.com>
2025-09-13 15:36:28 -0500

    feat(model,database) Begin the model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,20 +7,37 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     use HasFactory, HasRoles, Notifiable;
 
+    /**
+     * USER ATTRIBUTES
+     * $this->attributes['id'] - int - contains the user primary key (id)
+     * $this->attributes['name'] - string - contains the user name
+     * $this->attributes['email'] - string - contains the user email
+     * $this->attributes['password'] - string - contains the user password hashed
+     * $this->attributes['address'] - string - contains the user address
+     * $this->attributes['cardData'] - string - contains the user card data to process payments
+     * $this->orders - Order[] - contains the user's orders
+     * $this->reviews - Review[] - contains the user's reviews
+     * $this->tests - Test[] - contains the user's tests
+     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'address',
+        #'cardData',
     ];
 
     protected $hidden = [
         'password',
         'remember_token',
+        'cardData',
     ];
 
     protected function casts(): array
@@ -28,6 +45,27 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'cardData' => 'encrypted',
         ];
     }
+
+    getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    // public function orders(): HasMany
+    // {
+    //     return $this->hasMany(Order::class);
+    // }
+
+    // public function reviews(): HasMany
+    // {
+    //     return $this->hasMany(Review::class);
+    // }
+
+    // public function tests(): HasMany
+    // {
+    //     return $this->hasMany(Test::class);
+    // }
 }
